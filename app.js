987f2c955ccdcd8b3a4d46abe2e9968337fbdbae
kavania2002 <kavan155gondalia@gmail.com>
2021-10-23 00:43:50 +0530

    Convert into Node

--- app.js
+++ app.js
@@ -15,6 +15,26 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/flauntZone", (req, res) => {
+    res.render("flaunt_zone");
+});
+
+app.get("/diyAcademy", (req, res) => {
+    res.render("diy_academy");
+});
+
+app.get("/blogs", (req, res) => {
+    res.render("blogs");
+});
+
+app.get("/bazaar", (req, res) => {
+    res.render("bazaar");
+});
+
 app.get("/login", (req, res) => {
     res.render("login", {SAWO_API : SAWO_API});
 }); 
